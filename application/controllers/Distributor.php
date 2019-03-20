6a9d3c29cd172630589bb0b8f3af1a14b4ca9dbb
achyar <achyar@altermyth.com>
2019-03-20 14:45:39 +0700

    tambah distributor

---
+++ application/controllers/Distributor.php
@@ -0,0 +1,66 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Distributor extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		if(!$this->session->userdata('logged')) redirect('auth/login');
+		$this->load->model('Distributor_model');
+	}
+
+	public function index()
+	{
+
+		$this->load->library('pagination');
+		$page = $this->input->get('per_page');
+
+		$limit=5; 
+
+		if(!$page):
+			$offset = 0;
+		else:
+			$offset = $page;
+		endif;
+		
+		$config['page_query_string'] = TRUE;
+		$config['enable_query_strings'] = TRUE;
+		$config['query_string_segment'] = 'per_page';
+		$config['base_url'] = site_url('distributor');
+		$config['per_page'] = $limit;
+		$config['total_rows'] = $this->Distributor_model->get_distributor()->num_rows();
+		$this->pagination->initialize($config);
+
+		$data['jlhpage']= $page;
+		$data['distributor'] = $this->Distributor_model->get_distributor(null,$limit,$offset)->result();
+
+		$data['title'] = 'Distributor';
+		$data['main'] = 'distributor/index';
+		$this->load->view('templates/layout', $data);
+	}
+
+	function add(){
+		$params['distributor_name'] = $this->input->post('distributor_name');
+		$this->Distributor_model->insert_distributor($params);
+		redirect('distributor');
+	}
+
+	function edit(){
+		$id = $this->input->post('id');
+		$params['distributor_name'] = $this->input->post('distributor_name');
+		$this->Distributor_model->update_distributor($params, ['distributor_id'=>$id]);
+		redirect('distributor');
+	}
+
+
+    public function getubah()
+    {
+    	$id = $this->input->post('id');
+        echo json_encode($this->Distributor_model->get_distributor(['distributor_id'=>$id])->row_array());
+    }
+
+}
+
+/* End of file Distributor.php */
+/* Location: ./application/controllers/Distributor.php */
---
+++ application/views/distributor/index.php
@@ -0,0 +1,124 @@
+<div class="page-breadcrumb">
+  <div class="row align-items-center">
+    <div class="col-5">
+      <h4 class="page-title"><?php echo $title ?></h4>
+      <div class="d-flex align-items-center">
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="<?php echo site_url('/') ?>">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page"><?php echo $title ?></li>
+          </ol>
+        </nav>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-12">
+      <div class="card">
+        <div class="card-body">
+          <button type="button" class="btn btn-danger btn-sm float-right mb-3 tombolTambahData" data-toggle="modal" data-target="#formModal"><i class="fa fa-plus"></i> Tambah</button>
+          <div class="table-responsive">
+            <table class="table table-hover">
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Nama Distributor</th>
+                  <th>Tanggal Input</th>
+                  <th>Aksi</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                if (!empty($distributor)) {
+                  $i = $jlhpage+1;
+                  foreach ($distributor as $row):
+                    ?>
+                    <tr>
+                      <td><?php echo $i++ ?></td>
+                      <td><?php echo $row->distributor_name ?></td>
+                      <td><?php echo $row->distributor_created_at ?></td>
+                      <td>
+                        <a href="#" class="btn btn-success btn-sm tampilModalUbah" data-toggle="modal" data-target="#formModal" data-id="<?php echo $row->distributor_id; ?>">Edit</a>
+                      </td>
+                    </tr>
+                    <?php
+                  endforeach;
+                } else {
+                  ?>
+                  <tr id="row">
+                    <td colspan="3" align="center">Data Kosong</td>
+                  </tr>
+                <?php } ?>
+              </tbody>
+            </table>
+          </div>
+          <ul class="pagination">
+            <?php echo $this->pagination->create_links(); ?>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div id="formModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title" id="formModalLabel">Tambah Distributor</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+      </div>
+      <div class="modal-body">
+        <form action="<?php echo site_url('distributor/add') ?>" method="post">
+          <input type="hidden" name="id" id="id">
+          <div class="form-group">
+            <label>Nama Distributor</label>
+            <input type="text" class="form-control" id="name" name="distributor_name" autocomplete="off" required>
+          </div>
+
+        </div>
+        <div class="modal-footer">
+          <button type="submit" class="btn btn-success waves-effect">Tambah Data</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<script>
+
+  $(function(){
+
+    $('.tombolTambahData').on('click', function() {
+      $('#formModalLabel').html('Tambah Distributor');
+      $('.modal-footer button[type=submit]').html('Tambah Data');
+      $('.modal-body form').attr('action', '<?php echo site_url('distributor/add') ?>');
+      $('#name').val('');
+      $('#id').val('');
+    });
+
+    $('.tampilModalUbah').on('click', function() {
+
+      $('#formModalLabel').html('Ubah Data Distributor');
+      $('.modal-footer button[type=submit]').html('Ubah Data');
+      $('.modal-body form').attr('action', '<?php echo site_url('distributor/edit') ?>');
+
+      const id = $(this).data('id');
+
+      $.ajax({
+        url: '<?php echo site_url('distributor/getUbah') ?>',
+        data: {id : id},
+        method: 'post',
+        dataType: 'json',
+        success: function(data) {
+          $('#name').val(data.distributor_name);
+          $('#id').val(data.distributor_id);
+        }
+      });
+    });
+
+  });
+
+</script>
