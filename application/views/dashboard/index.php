a21775b5995be37d8466ef7bafad076ed587bf7b
achyar <achyar@altermyth.com>
2019-03-28 15:34:58 +0700

    layout dashboard

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,12 +1,12 @@
 <div class="page-breadcrumb">
   <div class="row align-items-center">
     <div class="col-5">
-      <h4 class="page-title">Starter Page</h4>
+      <h4 class="page-title"><?php echo $title ?></h4>
       <div class="d-flex align-items-center">
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Library</li>
+            <li class="breadcrumb-item active" aria-current="page"><?php echo $title ?></li>
           </ol>
         </nav>
       </div>
@@ -18,9 +18,65 @@
     <div class="col-12">
       <div class="card">
         <div class="card-body">
-          This is some text within a card block.
+          <div class="d-md-flex align-items-center">
+            <div>
+              <h4 class="card-title">Ringkasan Penjualan</h4>
+              <h5 class="card-subtitle">Bulan Terakhir</h5>
+            </div>
+            <div class="ml-auto d-flex no-block align-items-center">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-4">
+              <h1 class="m-b-0 m-t-30">Rp. 6,890.68</h1>
+              <h6 class="font-light text-muted">Current Month Earnings</h6>
+              <h3 class="m-t-30 m-b-0">1,540</h3>
+              <h6 class="font-light text-muted">Current Month Sales</h6>
+            </div>
+            <div class="col-lg-8">
+              <div class="campaign ct-charts"></div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card-body border-top">
+          <div class="row m-b-0">
+            <!-- col -->
+            <div class="col-lg-3 col-md-6">
+              <div class="d-flex align-items-center">
+                <div class="m-r-10"><span class="text-orange display-5"><i class="mdi mdi-wallet"></i></span></div>
+                <div><span>Wallet Balance</span>
+                  <h3 class="font-medium m-b-0">$3,567.53</h3>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+              <div class="d-flex align-items-center">
+                <div class="m-r-10"><span class="text-cyan display-5"><i class="mdi mdi-star-circle"></i></span></div>
+                <div><span>Referral Earnings</span>
+                  <h3 class="font-medium m-b-0">$769.08</h3>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+              <div class="d-flex align-items-center">
+                <div class="m-r-10"><span class="text-info display-5"><i class="mdi mdi-shopping"></i></span></div>
+                <div><span>Estimate Sales</span>
+                  <h3 class="font-medium m-b-0">5489</h3></div>
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-6">
+                <div class="d-flex align-items-center">
+                  <div class="m-r-10"><span class="text-primary display-5"><i class="mdi mdi-currency-usd"></i></span></div>
+                  <div><span>Earnings</span>
+                    <h3 class="font-medium m-b-0">$23,568.90</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
